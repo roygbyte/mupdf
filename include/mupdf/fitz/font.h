2944eff773e3151fbdf93aabb28651665177dbcb
Ez Diy <ezdiy@outlook.com>
2020-10-07 01:59:11 +0200

    Revert "External fonts .ttf loading fallback"
    This reverts commit 3a35e229f91b42506b17090a72b3f0cfd59ba11c.

--- include/mupdf/fitz/font.h
+++ include/mupdf/fitz/font.h
@@ -701,5 +701,5 @@ struct fz_font
 	unsigned char digest[16];
 };
 
-const unsigned char *fz_font_find_external(fz_context *ctx, const char *name, int nlen, int *size);
+const unsigned char *fz_font_find_external(fz_context *ctx, const char *name, int *size);
 #endif
