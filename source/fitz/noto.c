810545b6ba49fa34b81800ffd7c97d3789efbca8
Ez Diy <ezdiy@outlook.com>
2020-10-07 02:07:10 +0200

    NOBUILTINFONTS needs to be an ifdef

--- source/fitz/noto.c
+++ source/fitz/noto.c
@@ -72,6 +72,10 @@
 #define NOTO_TANGUT 0
 #endif
 
+#ifdef NOBUILTINFONTS
+#define RETURN(FORGE,NAME) \
+	return *size = 0, NULL;
+#else
 #ifdef HAVE_OBJCOPY
 #define RETURN(FORGE,NAME) \
 	do { \
@@ -89,6 +93,7 @@
 	extern unsigned int _binary_##NAME##_size; \
 	return *size = _binary_##NAME##_size, _binary_##NAME; \
 	} while (0)
+#endif
 #endif
 
 const unsigned char *
